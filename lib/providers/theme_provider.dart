1000509d703c894de33839a2494cf08a61b69afa
Amogh Vyas <amoghpvyas@gmail.com>
2026-04-12 00:20:48 +0530

    fix: definitively fix theme_provider.dart encoding

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// ThemeProvider manages the visual theme state (Dark/Light mode).
 class ThemeProvider extends ChangeNotifier {
   bool isDarkMode = true;
 
